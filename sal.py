544c75e5dd1e9c549111d53f330cbff9627764af
camilo muñoz <ingcamilo2022@gmail.com>
2022-04-26 23:06:05 -0500

    este es un mensaje de prueba

---
+++ sal.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 26 23:02:13 2022
+
+@author: User
+"""
+
+print("ingrese un nombre")
+nombre=input()
+print("ingrese un saludo")
+saludo=input()
+print(nombre+" "+saludo)
